ce692c19aa73187d08d191790266adb240cf6124
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2026-01-08 23:13:20 +0530

    basisc of node js

---
+++ tut-1/app.js
@@ -0,0 +1,24 @@
+require("./xyz");
+require("./sum");
+
+
+console.log("hi i am the entry point for node js.");
+
+// please run this file like node app.js
+
+//  and you see output like 
+
+/*
+    hi i am xyz module.
+    hi i am fom sum module.
+    hi i am the entry point for node js.
+*/
+
+//  to jo pahle import hota h vo file first run hoti hai 
+
+//  hamare case me pahle xyz module run hua then sum module 
+//  and last me hamari app.js file .
+
+// one importent thing i want to add here.
+// you can not access function and variables direclty from other file 
+// you have to use module.exports 
