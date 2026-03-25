669eadb488e897d5c5e7148a37d391427a34b1eb
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2026-03-25 09:04:33 +0530

    signup and login api include helper function

--- tut-24/Config/database.js
+++ tut-24/Config/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-    await mongoose.connect("mongodb+srv://sjava8643_db_user:UbvHHdpg4KbIsoa0@cluster0.dqcxlvs.mongodb.net/practiceDatabse-2");
+    await mongoose.connect("your_connection_key/practiceDatabse-2");
 }
